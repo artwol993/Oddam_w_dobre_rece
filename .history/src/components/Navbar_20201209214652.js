035947171ad6060a1c855da4b5ae2725a4ec8501
artwol993 <artwol993@gmail.com>
2020-12-10 00:06:11 +0100

    navbar done

---
+++ .history/src/components/Navbar_20201209214652.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../scss/navbar.scss';
+
+
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+
+
+
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+
+
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'x-icon' : 'bars'} />
+                    </div>
+                    <ul className={click ? 'active' : 'nav-menu'}>
+
+                        <li className='nav-items'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Start
+                            </Link>
+                        </li>
+                        <li className='nav-items'>
+                            <Link
+                                to='/'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                O co chodzi?
+                            </Link>
+                        </li>
+                        <li className='nav-items'>
+                            <Link
+                                to='/'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                O nas
+                            </Link>
+                        </li>
+
+                        <li className='nav-items'>
+                            <Link
+                                to='/'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                Fundacja i organizacje
+                            </Link>
+                        </li>
+                        <li className='nav-items'>
+                            <Link
+                                to='/'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                Kontakt
+                            </Link>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
---
+++ .history/src/components/Navbar_20201209223642.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../scss/navbar.scss';
+
+
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+if @media screen and(max-width)
+
+
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+
+
+                    <div className='menu-icon' onClick={handleClick}>
+                        <div className={click ? 'x-icon' : 'bars-icon'} />
+                    </div>
+
+                    <ul className={click ? 'nav-menu' : 'active'}>
+
+                        <li className='nav-items'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Start
+                            </Link>
+                        </li>
+                        <li className='nav-items'>
+                            <Link
+                                to='/'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                O co chodzi?
+                            </Link>
+                        </li>
+                        <li className='nav-items'>
+                            <Link
+                                to='/'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                O nas
+                            </Link>
+                        </li>
+
+                        <li className='nav-items'>
+                            <Link
+                                to='/'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                Fundacja i organizacje
+                            </Link>
+                        </li>
+                        <li className='nav-items'>
+                            <Link
+                                to='/'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                Kontakt
+                            </Link>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
