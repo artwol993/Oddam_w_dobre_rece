1e3c5980f93717011d04e555129e070ba58d8757
artwol993 <artwol993@gmail.com>
2021-04-13 21:37:34 +0200

    wwh section style done

---
+++ .history/src/components/pages/Home/WhoWeHelp/WhoWeHelp_20210413125239.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import "../../../../scss/components/WhoWeHelp.scss";
+import WwhList from './WwhList';
+
+
+function WhoWeHelp() {
+
+
+
+
+    return (
+        <>
+            <div className="wwh-container" id="who-we-help">
+                <h2>Komu pomagamy?</h2>
+                <div className="decoration"></div>
+                <div className="wwh-button-contaier">
+                    <button>Fundacjom</button>
+                    <button>Organizacjom poza rządowym</button>
+                    <button>Lokalnym zbiórkom</button>
+                </div>
+                <div className="wwh-info">
+                    W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.
+                </div>
+                <div className="wwh-list">
+                    <WwhList />
+                </div>
+
+
+
+            </div>
+
+        </>
+    )
+}
+
+export default WhoWeHelp
---
+++ .history/src/components/pages/Home/WhoWeHelp/WwhList_20210413125537.js
@@ -0,0 +1,145 @@
+import React, { Component } from 'react';
+import "../../../../scss/components/WwhList.scss";
+
+const LOAD_ROWS = 3;
+
+class WwhList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [],
+            currentPageElements: [],
+            renderComponent: false,
+        }
+
+    }
+
+    setPaginationElements() {
+        const paginationElements = []
+        const paginationTotalElements = Math.ceil(this.state.list.length / LOAD_ROWS)
+        for (let i = 0; i < paginationTotalElements; i++) {
+            paginationElements.push(
+                <div key={i}>
+                    {i + 1}
+                </div>
+            )
+        }
+        return (
+            <div className="pagination-elements">
+                {paginationElements}
+            </div>
+
+        )
+    }
+
+    fetchRemoteList() {
+        return fetch('http://localhost:3000/foundations/')
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    list: data
+                })
+
+                this.setCurrentPageElements()
+            })
+    }
+
+    setCurrentPageElements() {
+        for (let i = 0; i < LOAD_ROWS; i++) {
+            this.state.currentPageElements.push(this.state.list[i])
+        };
+        this.setState({
+            renderComponent: true
+        })
+    }
+
+    componentDidMount() {
+        this.fetchRemoteList()
+    }
+
+    renderFoundations() {
+        console.log(this.state.currentPageElements);
+        let paragraphs = [];
+        this.state.currentPageElements.forEach(el => {
+
+            console.log(el);
+            paragraphs.push(
+                <div className="wwh-list-row">
+                    <div className="wwh-list-row-box1">
+                        <h3 key={el.id}>Fundacja: {el.company}</h3>
+                        <div key={el.id}>{el.objective}</div>
+                    </div>
+                    <div className="wwh-list-row-box2" key={el.id}>{el.items}</div>
+                </div>
+
+            )
+        })
+        return paragraphs;
+    }
+
+    renderCurrentPageList() {
+        return (
+            <div className="wwh-list" id="wwh-foundation">
+
+                <div>
+                    {this.renderFoundations()}
+
+                </div>
+                <div className="decoration-line"></div>
+
+            </div>
+        )
+
+    }
+    render() {
+        if (!this.state.renderComponent) {
+            return ""
+        }
+        return (
+            <className=>
+                {
+                    this.renderCurrentPageList()
+                }
+                {
+                    this.setPaginationElements()
+
+                }
+            </className=>
+        )
+    }
+
+}
+
+export default WwhList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default WwhSlider;
